aa77315f09270f0b596cf113fc8d7394baeb1cc1
Danila <dod.shreder@gmail.com>
2026-02-22 17:18:17 +0300

    add admin interface

--- rating_site/admin.py
+++ rating_site/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, Review, FavouriteGame
+
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
@@ -7,3 +8,19 @@ class GameAdmin(admin.ModelAdmin):
     list_filter = ('genre', 'developer', 'year')
     search_fields = ('name', 'developer', 'producer')
     ordering = ('-year', 'name')
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('game', 'user', 'score')
+    list_filter = ('score', 'game')
+    search_fields = ('game__name', 'user__username')
+    ordering = ('game', 'user')
+
+
+@admin.register(FavouriteGame)
+class FavouriteGameAdmin(admin.ModelAdmin):
+    list_display = ('user', 'game', 'dt_created', 'dt_updated')
+    list_filter = ('dt_created', 'dt_updated')
+    search_fields = ('user__username', 'game__name')
+    ordering = ('-dt_created',)
