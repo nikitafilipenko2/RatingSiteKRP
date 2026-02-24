b461993114cc6510ea1ff410d5104d9c65ac6498
azerotde2004 <azerotde2004@gmail.com>
2026-02-24 20:57:43 +0300

    Создание вида списка игр, введение пагинации, базовый шаблон для страниц проекта, единая система навигации как шапка

--- rating_site/urls.py
+++ rating_site/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('games/', views.home, name='games_list'),  # временно ведет на home
+    path('game/<int:game_id>/', views.game_detail, name='game_detail'),
+    path('favourites/', views.favourites, name='favourites'),
+    path('add-to-favourite/<int:game_id>/', views.add_to_favourite, name='add_to_favourite'),
+    path('login/', auth_views.LoginView.as_view(template_name='rating_site/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- rating_site/views.py
+++ rating_site/views.py
@@ -1,11 +1,75 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .models import Game, FavouriteGame
+from django.core.paginator import Paginator
 from django.shortcuts import render
-
-from rating_site.models import Game
+from .models import Game
 
 
 def home(request):
-    games_list = Game.objects.all()
-    context = {
-        'games_list': games_list,
-    }
-    return render(request, 'home.html', context)
+    # Получаем все игры, сортируем по названию
+    games_list = Game.objects.all().order_by('name')
+
+    # Пагинация: 10 игр на страницу
+    paginator = Paginator(games_list, 10)
+    page_number = request.GET.get('page')
+    games = paginator.get_page(page_number)
+
+    return render(request, 'home.html', {
+        'games': games,
+        'title': 'Каталог игр'
+    })
+
+
+def game_detail(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    reviews = game.reviews.all()
+
+    return render(request, 'rating_site/game_detail.html', {
+        'game': game,
+        'reviews': reviews
+    })
+
+
+@login_required
+def favourites(request):
+    favourite_games = request.user.favourite_games.select_related('game').all()
+
+    return render(request, 'rating_site/favourites.html', {
+        'favourites': favourite_games
+    })
+
+
+@login_required
+def add_to_favourite(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+
+    # Проверяем, нет ли уже в избранном
+    favourite, created = FavouriteGame.objects.get_or_create(
+        user=request.user,
+        game=game
+    )
+
+    if created:
+        messages.success(request, f'Игра "{game.name}" добавлена в избранное')
+    else:
+        messages.info(request, f'Игра "{game.name}" уже в избранном')
+
+    return redirect('game_detail', game_id=game_id)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Регистрация успешна! Теперь вы можете войти.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'rating_site/register.html', {
+        'form': form
+    })
